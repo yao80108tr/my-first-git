763372c9e46536392a1add2754283b76e97a39ca
yao80108tr <yao80108tr@gmail.com>
2023-06-23 19:02:42 +0800

    初始化 Spring Boot

---
+++ demoMall/src/main/java/com/yao80108/demoMall/DemoMallApplication.java
@@ -0,0 +1,13 @@
+package com.yao80108.demoMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMallApplication.class, args);
+	}
+
+}
---
+++ demoMall/src/test/java/com/yao80108/demoMall/DemoMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yao80108.demoMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
